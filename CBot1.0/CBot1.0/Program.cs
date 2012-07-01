57f502ee5ef86be9bdc3009a4fc9def320f9f652
Oblivion <untitledblackness@gmail.com>
2012-07-01 16:58:55 +0200

    .

--- CBot1.0/CBot1.0/Program.cs
+++ CBot1.0/CBot1.0/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.IO;
 using System.Net.Sockets;
 using System.Threading;
-namespace irc
+namespace IRC
 {
     public class Client
     {
@@ -43,82 +43,4 @@ namespace irc
         }
     }
 
-    public class IrcCommand
-    {
-        Client client = new Client();
-
-        public string sender()
-        {
-            string raw = client.read();
-            string[] rawsp = raw.Split(new char[':'], 2); // ':', "user!ident@host etc"
-            string usermask = rawsp[1];
-            string[] user = usermask.Split(new char['!'], 2);// "user" "!ident@host etc"
-            string sender1 = user[0];
-            return sender1;
-        }
-
-        public string command()
-        {
-            string raw = client.read();
-            string[] rawsp = raw.Split(new char[':']); // ':', "user!ident@host" "command params"
-            if (rawsp.Length < 2)
-            {
-                string msg = rawsp[2];
-                string[] commandpar = msg.Split(new char[' '], 2); // "command" "par par par etc"
-                string command1 = commandpar[0];
-                return command1;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public string[] parameters()
-        {
-            string raw = client.read();
-            string[] rawsp = raw.Split(new char[':']);// ':', "user!ident@host" "cmd cmd cmd..."
-            if (rawsp.Length < 2)
-            {
-                string msg = rawsp[2];
-                string[] commandpar = msg.Split(new char[' '], 2); // "cmd" "params params params"
-                string par = commandpar[1];
-                string[] parameters1 = par.Split(new char[' ']); // "params" "params" "params" ...
-                return parameters1;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public string channel()
-        {
-            string raw = client.read();
-            string[] rawsp = raw.Split(new char[':']);
-            if (rawsp.Length < 2)
-            {
-                string ircraw = rawsp[1];
-                string[] r1 = ircraw.Split(new char[' ']); //
-                if (r1.Length < 2)
-                {
-                    string channel1 = r1[2];
-                    channel1.Replace(':', ' ');
-                    return channel1;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            else
-            {
-                return null;
-            }
-        }
-        public string pong()
-        {
-            return client.read().Split(new char[':'])[1].Split(new char[' '], 2)[1];
-        }
-    }
 }
